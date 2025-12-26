f833c729e660b0ff3410a48c406d5f452ff366aa
akhilscoderepo <153168600+akhilscoderepo@users.noreply.github.com>
2025-12-26 16:00:15 +0530

    Create 957-MinimumAddToMakeParenthesesValid

---
+++ 957-MinimumAddToMakeParenthesesValid/957-MinimumAddToMakeParenthesesValid.java
@@ -0,0 +1,20 @@
+// Last updated: 26/12/2025, 16:00:15
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int left=0;
+        int right=0;
+        
+        for(char c: s.toCharArray()){
+            if(c=='('){
+                right++;
+            }else if(right>0){
+                right--;
+            }else{
+                left++;
+            }
+        }
+       return left+right;
+    }
+}
+
+
