0615206ba92caaa8ad842f26af677954414f2a30
akhilscoderepo <153168600+akhilscoderepo@users.noreply.github.com>
2025-12-26 16:01:55 +0530

    Create 121-BestTimeToBuyAndSellStock

---
+++ 121-BestTimeToBuyAndSellStock/121-BestTimeToBuyAndSellStock.js
@@ -0,0 +1,20 @@
+// Last updated: 26/12/2025, 16:01:55
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let min = prices[0];
+    let max = 0;
+
+    for(let i = 0; i < prices.length; i++){
+        if(prices[i] < min){
+            min = prices[i];
+        }
+            max = Math.max(max, prices[i] - min)
+    }
+
+    return max;
+};
+
+
