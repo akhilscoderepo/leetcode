8169e20c133b69c1eac82fa178c2dbefb2406668
akhilscoderepo <153168600+akhilscoderepo@users.noreply.github.com>
2025-12-26 16:02:05 +0530

    Create 102-BinaryTreeLevelOrderTraversal

---
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,41 @@
+// Last updated: 26/12/2025, 16:02:04
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList();
+        if(root == null){return result;}
+        Queue<TreeNode> queue = new LinkedList();
+        queue.offer(root);
+
+        while(queue.size() > 0){
+            int level = queue.size();
+            List<Integer> currentLevel = new ArrayList();
+            for(int i =0; i<level; i++){
+                if(queue.peek().left != null){
+                    queue.offer(queue.peek().left);
+                }
+                if(queue.peek().right != null){
+                    queue.offer(queue.peek().right);
+                }
+                currentLevel.add(queue.poll().val);
+            }
+            result.add(currentLevel);
+        }
+
+    return result;
+    }
+}
