12d7e5c45a430fa115e1572e337916c5d1f8d29f
akhilscoderepo <153168600+akhilscoderepo@users.noreply.github.com>
2025-12-26 16:01:36 +0530

    Create 190-ReverseBits

---
+++ 190-ReverseBits/190-ReverseBits.java
@@ -0,0 +1,14 @@
+// Last updated: 26/12/2025, 16:01:35
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result=0;
+        
+        for(int i=0;i<32;i++){
+            result=(result<<1) | (n&1);
+            n>>=1;
+        }
+        return result;
+        
+    }
+}
