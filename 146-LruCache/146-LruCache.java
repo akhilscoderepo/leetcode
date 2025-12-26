a1116426122bfc3989ca9f809aa38e0552490cd9
akhilscoderepo <153168600+akhilscoderepo@users.noreply.github.com>
2025-12-26 16:01:46 +0530

    Create 146-LruCache

---
+++ 146-LruCache/146-LruCache.java
@@ -0,0 +1,36 @@
+// Last updated: 26/12/2025, 16:01:46
+class LRUCache {
+    LinkedHashMap<Integer,Integer> map = null;
+    int capacity=0;    
+    
+    public LRUCache(int capacity) {
+        map =new LinkedHashMap<Integer,Integer>();
+        this.capacity=capacity;
+    }
+    
+    public int get(int key) {
+        if(map.containsKey(key)){
+            int val = map.remove(key);
+            map.put(key,val);
+            return val;
+        }
+        return -1;
+    }
+    
+    public void put(int key, int value) {
+        if(map.containsKey(key)){
+            map.remove(key);
+        }else if(map.size()+1>capacity){
+            map.remove(map.keySet().iterator().next());
+        }
+        map.put(key,value);
+    }
+    
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
