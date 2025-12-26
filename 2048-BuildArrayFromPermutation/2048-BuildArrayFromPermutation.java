aa665de21a520022ee87b32b9852503faf2be845
akhilscoderepo <153168600+akhilscoderepo@users.noreply.github.com>
2025-12-26 15:58:58 +0530

    Create 2048-BuildArrayFromPermutation

---
+++ 2048-BuildArrayFromPermutation/2048-BuildArrayFromPermutation.java
@@ -0,0 +1,13 @@
+// Last updated: 26/12/2025, 15:58:58
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int length = nums.length;
+        int result[] = new int[length];
+        
+        for(int i=0; i<length; i++){
+            result[i] = nums[nums[i]];    
+        }
+        
+        return result;
+    }
+}
