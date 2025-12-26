0cfa22838efcdfd0d81a460c0155a17f698360c3
akhilscoderepo <153168600+akhilscoderepo@users.noreply.github.com>
2025-12-26 15:58:54 +0530

    Create 2137-FinalValueOfVariableAfterPerformingOperations

---
+++ 2137-FinalValueOfVariableAfterPerformingOperations/2137-FinalValueOfVariableAfterPerformingOperations.java
@@ -0,0 +1,16 @@
+// Last updated: 26/12/2025, 15:58:54
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int sum=0;
+        
+        for(int i=0;i<operations.length; i++){
+            if(operations[i].charAt(1)=='+'){
+                ++sum;
+            }else{
+                --sum;
+            }
+        }
+
+        return sum;
+    }
+}
