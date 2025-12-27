4f8af0feb16d4e1541bb4cd958d78927ba4919a3
Akhil <153168600+akhilscoderepo@users.noreply.github.com>
2025-12-27 14:11:16 +0530

    Create 100-SameTree

---
+++ 100-SameTree/version/java/100-SameTree_20251227_141115.java
@@ -0,0 +1,28 @@
+// Last updated: 27/12/2025, 14:11:15
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public boolean isSameTree(TreeNode p, TreeNode q) {
+18         
+19        if(p==null && q==null) return true;
+20        if((p==null && q!=null) || p!=null && q==null) return false;        
+21        if(p.val!=q.val) return false;
+22         return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+23    
+24    }
+25}
+26
+27
