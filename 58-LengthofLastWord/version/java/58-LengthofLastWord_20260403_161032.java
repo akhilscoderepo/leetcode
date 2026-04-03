b3a20d509095f06801f41e5044c9f0771d863504
Akhil <153168600+akhilscoderepo@users.noreply.github.com>
2026-04-03 16:10:33 +0530

    Create 58-LengthofLastWord

---
+++ 58-LengthofLastWord/version/java/58-LengthofLastWord_20260403_161032.java
@@ -0,0 +1,7 @@
+// Last updated: 03/04/2026, 16:10:32
+1class Solution {
+2    public int lengthOfLastWord(String s) {
+3        String[] split = s.split(" ");
+4        return split[split.length-1].length();
+5    }
+6}
